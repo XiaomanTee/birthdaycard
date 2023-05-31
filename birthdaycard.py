f0e3c8c94fca0d118866214c51dddbeb50a3018f
Xiao Man <ansontingxiaoman@ansons-macbook.home>
2023-06-01 00:07:56 +0100

    my first program - birthday card

---
+++ birthdaycard.py
@@ -0,0 +1,14 @@
+print("What's participant's name?")
+participant_name = input()
+print("Enter year of birth: ")
+year_of_birth = int(input())
+print("Enter personalized message: ")
+personalized_message = input()
+print("What's sender's name?")
+sender_name = input()
+
+age = 2023 - year_of_birth
+
+print(f"{participant_name}, let's celebrate your {age} years of awesomeness!\nWishing you a day filled with joy and laughter as you turn {age}!")
+print(f"\n{personalized_message}\n")
+print(f"With love and best wishes,\n{sender_name}")
